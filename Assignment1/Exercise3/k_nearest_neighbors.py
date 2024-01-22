6a4a2b899565f2c627747dcccae3bfaa4fd1894b
j-taylor-pixel <68829110+j-taylor-pixel@users.noreply.github.com>
2024-01-22 18:17:11 +0000

    added all test cases

--- Assignment1/Exercise3/k_nearest_neighbors.py
+++ Assignment1/Exercise3/k_nearest_neighbors.py
@@ -24,7 +24,7 @@ def euclidian_distance(x,y):
 
     return total ** 0.5
 
-def read_csv(filename='A1Q4NearestNeighbors.csv'):
+def read_csv(filename='Assignment1/Exercise3/A1Q4NearestNeighbors.csv'):
     file = open(filename)
     #type(file)
     csvreader = csv.reader(file)
@@ -50,16 +50,9 @@ def split_rows(rows, train_count, val_count, test_count):
     val_rows = rows[train_count:train_count + val_count]
     test_rows = rows[train_count + val_count:]
     return train_rows, val_rows, test_rows
-    
 
-def k_nearest_neighbors(k=1, metric=DistanceTypes.cosine, train_size=80, val_size=10, test_size=10):
 
-    rows = read_csv()
-    rows = string_to_float(rows)
-    train_count, val_count, test_count = splice_data(num_rows=len(rows),train_size=train_size, val_size=val_size, test_size=test_size)
-    train_rows, val_rows, test_rows = split_rows(rows, train_count, val_count, test_count)
-
-    
+def performance(k, test_rows, train_rows, metric):
     # loop thru test data
     correct_count = wrong_count = 0
     for test_row in test_rows:
@@ -85,10 +78,31 @@ def k_nearest_neighbors(k=1, metric=DistanceTypes.cosine, train_size=80, val_siz
             wrong_count += 1
     return correct_count / (correct_count + wrong_count) # return accuracy
 
-print(k_nearest_neighbors(k=25, metric=DistanceTypes.euclidian))
+def k_nearest_neighbors(k=1, metric=DistanceTypes.cosine, train_size=80, val_size=10, test_size=10):
 
-# data split can start with 80/10/10
+    rows = read_csv()
+    rows = string_to_float(rows)
+    train_count, val_count, test_count = splice_data(num_rows=len(rows),train_size=train_size, val_size=val_size, test_size=test_size)
+    train_rows, val_rows, test_rows = split_rows(rows, train_count, val_count, test_count)
 
+    val_perf = performance(k=k,test_rows=val_rows,train_rows=train_rows, metric=metric)
+    test_perf = performance(k=k,test_rows=test_rows,train_rows=train_rows, metric=metric)
+    return val_perf, test_perf
+
+print(k_nearest_neighbors(k=1, metric=DistanceTypes.cosine))
+
+# for each metric
+metrics = [DistanceTypes.cosine, DistanceTypes.euclidian]
+data_splits = [[80,10,10],[34,33,33],[25,25,50]]
+k_values = [1,3,5,11]
+for metric in metrics:
+    for data_split in data_splits:
+        for k in k_values:
+            val_perf, test_perf = k_nearest_neighbors(k=k, metric=metric, train_size=data_split[0], val_size=data_split[2], test_size=data_split[1])
+            #print(f"Results: metric: {metric}, data split: {data_split}, k: {k}, val_perf: {val_perf}, test_perf: {test_perf}")
+            # consider storing everything and making averages or plot or something
+# best result is: metric: euclidian, data split: [34, 33, 33], k: 1, val_perf: 0.6614173228346457, test_perf: 0.6746031746031746
+# need to somehow choose best point from a gazillion values
 
     # column attributes are the following
     #   1. Number of times pregnant
